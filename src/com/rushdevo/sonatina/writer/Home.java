bb242977a6b9a450a547a3f3883ec6dd9428a4f8
Jason Rush <diminish7@gmail.com>
2011-06-03 13:01:35 -0600

    added home view

--- src/com/rushdevo/sonatina/writer/Home.java
+++ src/com/rushdevo/sonatina/writer/Home.java
@@ -2,12 +2,39 @@ package com.rushdevo.sonatina.writer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class Home extends Activity {
-    /** Called when the activity is first created. */
+/**
+ * @author jasonrush
+ *
+ *	The home screen activity
+ */
+public class Home extends Activity implements OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.home);
+        View newButton = findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+        View openButton = findViewById(R.id.open_button);
+        openButton.setOnClickListener(this);
+        View exitButton = findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
     }
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.new_button:
+    		// TODO
+    		break;
+		case R.id.open_button:
+			// TODO
+			break;
+		case R.id.exit_button:
+			finish();
+			break;
+		}
+	}
 }
